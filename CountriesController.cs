aee11e5c25ba8fd284c03467b5929678c529fe4b
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-06-24 11:51:53 +0300

    Add project files.

---
+++ CountriesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfHostedAppNetCore
+{
+    [Route("countries")]
+    public class CountriesController
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] {"Japan", "China"};
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SelfHostedAppNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder()
+                .ConfigureServices(services => services.AddMvc())
+                .Configure(app => app.UseMvc())
+                .Build()
+                .Run();
+        }
+    }
+}
